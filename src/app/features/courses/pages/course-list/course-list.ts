d2096667e362e732b85d60d8fae493282877af72
kolio <odysseas.kolios.com>
2025-12-19 20:10:13 +0200

    feat: courses list UI with mock data

--- src/app/features/courses/pages/course-list/course-list.ts
+++ src/app/features/courses/pages/course-list/course-list.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+type CourseVm = {
+  id: number;
+  title: string;
+  credits: number;
+  startDate: string;
+  endDate: string;
+  teacherId?: number | null;
+};
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './course-list.html',
   styleUrl: './course-list.scss',
 })
 export class CourseList {
+  courses: CourseVm[] = [
+    {
+      id: 1,
+      title: 'Intro to C#',
+      credits: 5,
+      startDate: '2025-10-01',
+      endDate: '2025-12-15',
+      teacherId: 2,
+    },
+    {
+      id: 2,
+      title: 'Databases (SQL Server)',
+      credits: 6,
+      startDate: '2025-10-10',
+      endDate: '2026-01-20',
+      teacherId: null,
+    },
+  ];
 
+  onDelete(courseId: number) {
+    // placeholder: αργότερα θα καλεί API
+    const ok = confirm(`Delete course with id ${courseId}?`);
+    if (!ok) return;
+
+    this.courses = this.courses.filter(c => c.id !== courseId);
+  }
 }
