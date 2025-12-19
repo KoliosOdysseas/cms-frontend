f7df62ac27c431cfb2e02f192f7912bd2ee88db9
kolio <odysseas.kolios.com>
2025-12-19 20:04:22 +0200

    feat: basic layout, routing and pages skeleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CourseList } from './features/courses/pages/course-list/course-list';
+import { CourseForm } from './features/courses/pages/course-form/course-form';
+import { TeacherList } from './features/teachers/pages/teacher-list/teacher-list';
+import { TeacherForm } from './features/teachers/pages/teacher-form/teacher-form';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+
+  { path: 'courses', component: CourseList },
+  { path: 'courses/new', component: CourseForm },
+  { path: 'courses/:id/edit', component: CourseForm },
+
+  { path: 'teachers', component: TeacherList },
+  { path: 'teachers/new', component: TeacherForm },
+  { path: 'teachers/:id/edit', component: TeacherForm },
+
+  { path: '**', redirectTo: 'courses' },
+];
---
+++ src/app/features/teachers/pages/teacher-form/teacher-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-form',
+  imports: [],
+  templateUrl: './teacher-form.html',
+  styleUrl: './teacher-form.scss',
+})
+export class TeacherForm {
+
+}
