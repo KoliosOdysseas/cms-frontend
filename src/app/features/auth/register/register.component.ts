0eb24e0c5fe7c3fd300ed93b11ab3a2a1822772d
kolio <odysseas.kolios.com>
2025-12-26 22:31:09 +0200

    feat(auth): enhance registration error handling and success message

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router, } from '@angular/router';
 
 import { AuthApiService, UserRole } from '../../../core/services/auth-api.service';
 
